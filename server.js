b64669538cd4a014e032144e981b8768af0ea081
vinishvats14 <vvinishvats@gmail.com>
2025-10-29 10:19:49 +0530

    new changes

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express() ;
 const port = process.env.PORT || 3001 ;
 
 app.use(cors()) ;
+
+app.use(cors({
+  origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+  credentials: true,
+}));
+
 app.use(express.json()) ;
 
 app.get('/', (req, res) => {
